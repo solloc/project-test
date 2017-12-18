9fbcdb7c2fbe8f80040e5db164a2d1206e642160
solloc <matthias@maisch.me>
2017-12-18 17:15:29 +0100

    add and navigate to details (show) added

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,5 +1,30 @@
 class IncidentsController < ApplicationController
   def index
-    @incidents = Incident.all
+    @incidents = Incident.all.reverse_order!
   end
+
+  def new
+    @incident = Incident.new
+  end
+
+  def create
+    @incident = Incident.new(incident_params)
+    if @incident.save
+      redirect_to(incidents_path)
+    else
+      redirect_to(incidents/new_path)
+    end
+  end
+
+  def show
+    @incident = Incident.find(params[:id])
+  end
+
+
+  private
+  def incident_params
+    params.require(:incident).permit(:priority, :title)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'incidents' => 'incidents#index'
+  get 'incidents/new' => 'incidents#new'
+  get 'incidents/:id' => 'incidents#show', as: 'incident'
+  post 'incidents' => 'incidents#create'
   root 'pages#index'
   get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
