f729c0e41894b5646d0de9efb23c2bec448c1288
solloc <matthias@maisch.me>
2017-12-19 01:23:16 +0100

    initial user model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_12_07_191659) do
+ActiveRecord::Schema.define(version: 2017_12_18_235254) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2017_12_07_191659) do
     t.string "title"
     t.string "source"
     t.integer "priority"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
